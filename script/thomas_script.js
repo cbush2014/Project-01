8ecb342dbec43821955a8a63878697d15482c103
Th3freeman <thefrman345@gmail.com>
2019-11-06 01:40:53 -0500

    changed font size to vw and added some bulma classes to make the battle page more responsive on small screens, adjusted some text alignments in index.html and changed the link hover color to make it more noticeable. fixed an issue with the background not displaying properly on small screens on the battlepage. added an area to display the round conclusion every round. added random monster portraits that change with every monster

--- script/thomas_script.js
+++ script/thomas_script.js
@@ -1,14 +1,14 @@
 $("document").ready(generateMonInsult());
 // var victoryCounter = 0;//-- a way to keep track of the wins so we can progress to the next difficulty
-
+var monsterPortraits = ["./assets/images/mon1.jpg", "./assets/images/mon2.jpg", "./assets/images/mon3.jpg", "./assets/images/mon4.jpg", "./assets/images/mon5.jpg", "./assets/images/mon6.png", "./assets/images/mon7.jpg", "./assets/images/mon8.jpg"]
 Barbar = {
     hitpoints: 37,
-    damage: 11,
+    damage: Math.floor(Math.random() * 10 + 8),
 }
 
 var Monster = getRandomBattleMonsterFromArray(easy);
 var MonsterHealth = Monster.hit_points;
-var monDmg = Math.floor(Math.random() + 7);
+var monDmg = Math.floor(Math.random() * 10 + 5);
 var monName = Monster.name;
 $("#monName").text(monName);
 
@@ -19,11 +19,12 @@ function defeat() {
 function battleTime() {
     var Monster = getRandomBattleMonsterFromArray(easy);
     var MonsterHealth = Monster.hit_points;
-    var monDmg = Math.floor(Math.random() + 7);
+    var monDmg = Math.floor(Math.random() * 8 + 5);
     var monName = Monster.name;
     $("#monHealth").text(MonsterHealth);
     $("#monName").text(monName);
     $("#playHealth").text(BarHealth);
+    $("#monPortrait").attr("src", monsterPortraits[Math.floor(Math.random() * monsterPortraits.length)]);
     return MonsterHealth, monDmg;
 }
 
@@ -38,21 +39,23 @@ $("#playHealth").text(BarHealth);
 $("#monHealth").text(MonsterHealth);
 
 rockPS = function (guess) {//-- the Battle System; rock, paper, scissors basically.
-    var monsterAttacks = ["f", "s", "c"];
+    var monsterAttacks = ["Fast Attack", "Strong Attack", "Counter"];
 
     // Randomly chooses a choice from the options array. This is the Computer's guess.
     var computerGuess = monsterAttacks[Math.floor(Math.random() * monsterAttacks.length)];
     var userGuess = guess;
     // Win/lose conditions:
-    if ((userGuess === "f" && computerGuess === "s") ||
-        (userGuess === "s" && computerGuess === "c") ||
-        (userGuess === "c" && computerGuess === "f")) {
+    if ((userGuess === "Fast Attack" && computerGuess === "Strong Attack") ||
+        (userGuess === "Strong Attack" && computerGuess === "Counter") ||
+        (userGuess === "Counter" && computerGuess === "Fast Attack")) {
         $('audio#barb')[0].play();
         var curMONHealth = $("#monHealth").text() - barDmg;
         $("#monHealth").text(curMONHealth);
         if (curMONHealth = 0 || curMONHealth < 0) {
             battleTime();
         }
+        $("#choiceDisplay").empty();
+        $("#choiceDisplay").text("Axe used " + userGuess + ". The Monster used " + computerGuess + ". Axe dealt " + barDmg + " damage to the Monster");
 
     } else if (userGuess === computerGuess) {
         $('audio#barbdamaged')[0].play();
@@ -66,6 +69,8 @@ rockPS = function (guess) {//-- the Battle System; rock, paper, scissors basical
         if (curPlayHealth = 0 || curPlayHealth < 0) {
             defeat();
         }
+        $("#choiceDisplay").empty();
+        $("#choiceDisplay").text("Axe used " + userGuess + ". The Monster used " + computerGuess + ". Axe takes " + Math.floor(monDmg / 2) + " damage. The Monster takes " + barDmg / 2 + " damage.");
 
     } else {
         $('audio#barbdamaged')[0].play();
@@ -74,7 +79,12 @@ rockPS = function (guess) {//-- the Battle System; rock, paper, scissors basical
         if (curPlayHealth = 0 || curPlayHealth < 0) {
             defeat();
         }
+        $("#choiceDisplay").empty();
+        $("#choiceDisplay").text("Axe used " + userGuess + ". The Monster used " + computerGuess + ". Axe takes " + Math.floor(monDmg) + " damage.");
     }
+    console.log(userGuess);
+    console.log(computerGuess);
+    // $("#choiceDisplay").text("Axe used "+userGuess+". "+monName+" used "+computerGuess+".");
 }
 
 // Taunt Button
@@ -103,17 +113,19 @@ function generateMonInsult() {
 function CleanInsult(reallybadphrase) {
 
     var str = reallybadphrase
-    str = str.replace(/whore's soiled tampon/i,"call girl's soiled Napkin.")
+    str = str.replace(/whore's soiled tampon/i, "call girl's soiled Napkin.")
     str = str.replace(/motherfucker/gi, "goofball")
-    str = str.replace("asshole","backend");
-    str = str.replace(/testicles/gi,"privates")
+    str = str.replace("asshole", "backend");
+    str = str.replace(/testicles/gi, "privates")
     str = str.replace(/fuck/gi, "jerk")
     str = str.replace(/&quot;/gi, "'")
     str = str.replace(/&gt;/gi, ">")
-    str = str.replace(/ass/gi,"dork")
-    str = str.replace(/&amp;/gi,"&")
-    str = str.replace(/dick/gi,"willy")
-    str = str.replace(/crap/gi,"poop")
-    return str;    
+    str = str.replace(/ass/gi, "dork")
+    str = str.replace(/&amp;/gi, "&")
+    str = str.replace(/dick/gi, "willy")
+    str = str.replace(/crap/gi, "poop")
+    return str;
 }
 
+
+$("#monPortrait").attr("src", monsterPortraits[Math.floor(Math.random() * monsterPortraits.length)]);
